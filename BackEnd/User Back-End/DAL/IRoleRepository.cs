28b4d69ae90c8da0bf6be37db8768a957cc2d7c0
Dylan <74596681+CBOnyx@users.noreply.github.com>
2022-03-15 10:52:57 +0100

    Changes classes en migrations
    Role klasses toegevoegd, en migrations gedaan voor alle tabellen

---
+++ BackEnd/User Back-End/DAL/IRoleRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace User_Back_End.DAL
+{
+    public class IRoleRepository
+    {
+    }
+}
---
+++ BackEnd/User Back-End/Models/Role.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace User_Back_End.Models
+{
+    public class Role
+    {
+    }
+}
