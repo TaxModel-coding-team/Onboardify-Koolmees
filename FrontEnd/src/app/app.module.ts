6bb635311e0acaabd369db4b3406b089bd4b12dd
Stefan de Graaf <stefan.de.graaf.013@hotmail.com>
2022-04-05 15:39:26 +0200

    Tab element added to badges and achievements component. :sparkles:

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -1,38 +1,35 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
-import { GlassModule } from 'angular-glass';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { QuestsComponent } from './quests/quests.component';
-import { ProgressBarModule } from 'angular-progress-bar';
-import { MsalModule, MsalService, MSAL_INSTANCE } from '@azure/msal-angular';
-import { IPublicClientApplication, PublicClientApplication } from '@azure/msal-browser';
-import { MicrosoftLoginComponent } from './microsoft-login/microsoft-login.component';
-import { RegistrationComponent } from './Registration/registration.component';
-import { FormsModule } from '@angular/forms';
-import { ProfilePageComponent } from './profile-page/profile-page.component';
-import { HeaderComponent } from './header/header.component';
-import { BadgesComponent } from './badges/badges.component';
-import { AchievementsComponent } from './achievements/achievements.component';
-import { ProfileDetailsComponent } from './profile-details/profile-details.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { RouterModule } from '@angular/router';
-import { QRCode } from 'qrcode';
-import { ScanFolioCitaComponent } from './scan-folio-cita/scan-folio-cita.component';
-import { ZXingScannerModule } from '@zxing/ngx-scanner';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
+import {GlassModule} from 'angular-glass';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {QuestsComponent} from './quests/quests.component';
+import {ProgressBarModule} from 'angular-progress-bar';
+import {MsalModule, MsalService, MSAL_INSTANCE} from '@azure/msal-angular';
+import {IPublicClientApplication, PublicClientApplication} from '@azure/msal-browser';
+import {MicrosoftLoginComponent} from './microsoft-login/microsoft-login.component';
+import {RegistrationComponent} from './Registration/registration.component';
+import {FormsModule} from '@angular/forms';
+import {ProfilePageComponent} from './profile-page/profile-page.component';
+import {HeaderComponent} from './header/header.component';
+import {BadgesComponent} from './badges/badges.component';
+import {AchievementsComponent} from './achievements/achievements.component';
+import {ProfileDetailsComponent} from './profile-details/profile-details.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {RouterModule} from '@angular/router';
+import {QRCode} from 'qrcode';
+import {ZXingScannerModule} from '@zxing/ngx-scanner';
 
 
-export function MSALInstanceFactory(): IPublicClientApplication{
+export function MSALInstanceFactory(): IPublicClientApplication {
   return new PublicClientApplication({
     auth: {
       clientId: '455b6cce-e291-46f6-a7d8-1dfecbe011da',
       redirectUri: 'http://localhost:4200'
-
     }
   })
 }
-
 
 @NgModule({
   declarations: [
@@ -45,8 +42,6 @@ export function MSALInstanceFactory(): IPublicClientApplication{
     BadgesComponent,
     AchievementsComponent,
     ProfileDetailsComponent,
-    
-    
   ],
   imports: [
     BrowserModule,
@@ -59,7 +54,6 @@ export function MSALInstanceFactory(): IPublicClientApplication{
     NgbModule,
     RouterModule,
     ZXingScannerModule
-    
   ],
   providers: [
     {
@@ -70,4 +64,5 @@ export function MSALInstanceFactory(): IPublicClientApplication{
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
