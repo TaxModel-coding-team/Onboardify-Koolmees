ac2f6c1cf2e1f2fdbbb70f142d4a8bec8b3f92d8
Raoul Goorden <rgoorden@t-mobilethuis.nl>
2022-06-19 20:47:33 +0200

    testing

---
+++ BackEnd/TestProject/Stub/QuestStub.cs
@@ -0,0 +1,17 @@
+﻿using back_end.DAL;
+using back_end.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestProject.Stub
+{
+    class QuestStub : IQuestRepository
+    {
+        private List<Quest> quests = new List<Quest>();
+        public QuestStub()
+        {
+            CreateQuests();
+        }
+    }
+}
---
+++ BackEnd/TestProject/Tests/QuestTests.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using back_end.DAL;
+using back_end.Logic;
+using back_end.Models;
+using back_end.ViewModels;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TestProject.Stub;
+
+namespace TestProject.Tests
+{
+    [TestClass]
+    public class QuestTests
+    {
+        private readonly QuestLogic _logic;
+        private readonly IMapper _mapper;
+
+        public QuestTests()
+        {
+            MapperConfiguration config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<Quest, QuestViewModel>().ReverseMap();
+                cfg.CreateMap<SubQuest, SubQuestViewModel>().ReverseMap();
+                cfg.CreateMap<QuestUserManagement, QuestCompletionViewModel>().ReverseMap();
+                cfg.CreateMap<List<SubQuest>, List<SubQuestViewModel>>().ReverseMap();
+            });
+
+            _mapper = new Mapper(config);
+            IQuestRepository IQuestRepository = new QuestStub();
+            _logic = new QuestLogic(IQuestRepository, _mapper);
+        }
+    }
+}
